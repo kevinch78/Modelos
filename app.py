1b756e9c73b82f9081879ca406c047ef8a11d4cb
zare <zarellaburbanov@gmail.com>
2025-05-22 14:09:21 -0500

    front

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 import joblib
 import pandas as pd
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='front')
 
 # Carga modelos y columnas
 model_knn = joblib.load('modelo_knn.pkl')
 model_rf = joblib.load('modelo_random_forest.pkl')
 columns = joblib.load('columns.pkl')  # ['OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF']
 
-@app.route('/predict', methods=['POST'])
+@app.route('/')
+def index():
+    return send_from_directory('front', 'index.html')
 
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory('front', path)
+
+@app.route('/predict', methods=['POST'])
 def predict():
     print("Columnas que espera el modelo:", columns)
 
@@ -32,8 +39,6 @@ def predict():
     except Exception as e:
         print("❌ Error:", e)
         return jsonify({"error": str(e)}), 400
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
